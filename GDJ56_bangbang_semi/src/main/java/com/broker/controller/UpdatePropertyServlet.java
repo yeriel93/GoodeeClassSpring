3c20c820a0c96076e3ef63e350355973a51a7c3e
yeriel93 <yeonjilim20@gmail.com>
2022-12-16 01:43:39 +0900

    2022-12-15 [YJ] 매물수정페이지 관리비,공실예정일,옵션,파일제외 데이터 불러오기 완료 & 매물등록페이지 상세페이지,전/월세 부분 수정

--- GDJ56_bangbang_semi/src/main/java/com/broker/controller/UpdatePropertyServlet.java
+++ GDJ56_bangbang_semi/src/main/java/com/broker/controller/UpdatePropertyServlet.java
@@ -1,11 +1,15 @@
 package com.broker.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.property.model.vo.Property;
+import com.property.service.PropertyService;
 
 
 @WebServlet("/account/broker/updateProperty.bb")
@@ -15,17 +19,17 @@ public class UpdatePropertyServlet extends HttpServlet {
     public UpdatePropertyServlet() {
         super();
     }
-
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		//임시로 매물번호를 받아서 데이터 불러옴 -> 나중에 수정필요
+		//매물리스트에서 통해서 넘어오는 페이지니 거기서 매물 객체전체를 넘기면 더 좋지 않을까? 생각 좀 더 필요
+		int propertyNo = 300042;
+		Property property = PropertyService.getPropertyService().searchPropertyOne(propertyNo);
+//		System.out.println("UpdateProperty서블릿: "+property);
 		
 		
-		
-		
-		
-		
-		
-		
+		request.setAttribute("property", property);
 		request.getRequestDispatcher("/views/account/broker/updateProperty.jsp").forward(request, response);
 	}
 
